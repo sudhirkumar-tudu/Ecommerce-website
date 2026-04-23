568b505f158d984e9a7cc42a8eb23fc2102ba18d
naikswatej-sudo <naikswatej@gmail.com>
2026-04-23 19:38:53 +0530

    Delete Backend directory

--- Backend/src/middleware/notFound.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response} from "express";
-import { fail } from "../utils/envelope";
-
-export function notFound(req:Request, res: Response){
-  res.status(404).json(
-    fail(`Route not found ${req.method}`)
-  )
-}
